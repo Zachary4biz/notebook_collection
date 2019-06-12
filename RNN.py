f6489fd73bb7c0ec4d920629cee32d42413b8e9a
zhoutong <zachary4biz@yeah.net>
2019-06-12 20:39:55 +0800

    update

--- RNN.py
+++ RNN.py
@@ -24,8 +24,125 @@ import matplotlib.pyplot as plt
 # # 参考
 # - [莫烦 RNN on Tensorflow](https://morvanzhou.github.io/tutorials/machine-learning/tensorflow/5-08-RNN2/#%E5%AE%9A%E4%B9%89-RNN-%E7%9A%84%E4%B8%BB%E4%BD%93%E7%BB%93%E6%9E%84)
 # - [莫烦 RNN on Pytorch](https://morvanzhou.github.io/tutorials/machine-learning/torch/4-02-RNN-classification/)
+# - [Tensorflow 官方教程](https://www.tensorflow.org/tutorials/sequences/recurrent?hl=zh-cn)
 
-# # Pytorch
+# # Morvan | Tensorflow
+
+# In[3]:
+
+
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+tf.set_random_seed(1)   # set random seed
+
+
+# ## Data Prepare
+
+# In[3]:
+
+
+# 导入数据
+mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+
+
+# ## Model
+
+# ### 超参数
+
+# In[4]:
+
+
+# hyperparameters
+lr = 0.001                  # learning rate
+training_iters = 100000     # train step 上限
+batch_size = 128            
+n_inputs = 28               # MNIST data input (img shape: 28*28)
+n_steps = 28                # time steps
+n_hidden_units = 128        # neurons in hidden layer
+n_classes = 10              # MNIST classes (0-9 digits)
+
+
+# ### RNN建立计算图
+
+# In[5]:
+
+
+# x y placeholder
+x = tf.placeholder(tf.float32, [None, n_steps, n_inputs])
+y = tf.placeholder(tf.float32, [None, n_classes])
+
+# 对 weights biases 初始值的定义
+weights = {
+    # shape (28, 128)
+    'in': tf.Variable(tf.random_normal([n_inputs, n_hidden_units])),
+    # shape (128, 10)
+    'out': tf.Variable(tf.random_normal([n_hidden_units, n_classes]))
+}
+biases = {
+    # shape (128, )
+    'in': tf.Variable(tf.constant(0.1, shape=[n_hidden_units, ])),
+    # shape (10, )
+    'out': tf.Variable(tf.constant(0.1, shape=[n_classes, ]))
+}
+
+
+# In[7]:
+
+
+def RNN(X, weights, biases):
+    # 原始的 X 是 3 维数据, 我们需要把它变成 2 维数据才能使用 weights 的矩阵乘法
+    # X ==> (128 batches * 28 steps, 28 inputs)
+    X = tf.reshape(X, [-1, n_inputs])
+
+    # X_in = W*X + b
+    X_in = tf.matmul(X, weights['in']) + biases['in']
+    # X_in ==> (128 batches, 28 steps, 128 hidden) 换回3维
+    X_in = tf.reshape(X_in, [-1, n_steps, n_hidden_units])
+    
+    # 使用 basic LSTM Cell.
+    lstm_cell = tf.contrib.rnn.BasicLSTMCell(n_hidden_units, forget_bias=1.0, state_is_tuple=True)
+    init_state = lstm_cell.zero_state(batch_size, dtype=tf.float32) # 初始化全零 state
+
+    # 如果使用tf.nn.dynamic_rnn(cell, inputs), 我们要确定 inputs 的格式. tf.nn.dynamic_rnn 中的 time_major 参数会针对不同 inputs 格式有不同的值.
+    # 如果 inputs 为 (batches, steps, inputs) ==> time_major=False;
+    # 如果 inputs 为 (steps, batches, inputs) ==> time_major=True;
+    outputs, final_state = tf.nn.dynamic_rnn(lstm_cell, X_in, initial_state=init_state, time_major=False)
+    
+    results = tf.matmul(final_state[1], weights['out']) + biases['out']
+    return results
+
+
+# ## Fit
+
+# In[8]:
+
+
+pred = RNN(x, weights, biases)
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(pred, y))
+train_op = tf.train.AdamOptimizer(lr).minimize(cost)
+correct_pred = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+
+init = tf.global_variables_initializer()
+with tf.Session() as sess:
+    sess.run(init)
+    step = 0
+    while step * batch_size < training_iters:
+        batch_xs, batch_ys = mnist.train.next_batch(batch_size)
+        batch_xs = batch_xs.reshape([batch_size, n_steps, n_inputs])
+        sess.run([train_op], feed_dict={
+            x: batch_xs,
+            y: batch_ys,
+        })
+        if step % 20 == 0:
+            print(sess.run(accuracy, feed_dict={
+            x: batch_xs,
+            y: batch_ys,
+        }))
+        step += 1
+
+
+# # Morvan | Pytorch
 
 # In[41]:
 
@@ -72,7 +189,6 @@ plt.show()
 
 
 # 批训练 50samples, 1 channel, 28x28 (50, 1, 28, 28)
-
 test_x = test_data.data.type(torch.FloatTensor)[:2000]   # shape (2000, 28, 28) 
 test_x = test_x/255. # normalize to range(0,1)
 test_y = test_data.targets.numpy()[:2000]    # covert to numpy array
@@ -175,6 +291,178 @@ test_output = rnn(test_x[:10].view(-1, 28, 28))
 pred_y = torch.max(test_output, 1)[1].data.numpy()
 print('pred number: ', pred_y)
 print('true number: ', test_y[:10])
+
+
+# # Tensorflow Official
+
+# In[10]:
+
+
+import collections
+
+
+# In[15]:
+
+
+list(collections.OrderedDict.fromkeys([1,2,3,4,44,56,7,87,4,4,5,6,5]).keys())
+
+
+# ## Prepare
+
+# In[18]:
+
+
+import reader
+raw_data = reader.ptb_raw_data("/home/zhoutong/data/PTB/simple-examples/data")
+train_data, valid_data, test_data, _ = raw_data
+
+
+# In[19]:
+
+
+# 数据量太大，继续抽小样本
+train_data, valid_data, test_data = train_data[:100], valid_data[:20], test_data[:20]
+len(train_data)
+
+
+# In[21]:
+
+
+class PTBInput(object):
+    """The input data."""
+
+    def __init__(self, config, data, name=None):
+        self.batch_size = batch_size = config.batch_size
+        self.num_steps = num_steps = config.num_steps
+        self.epoch_size = ((len(data) // batch_size) - 1) // num_steps
+        self.input_data, self.targets = reader.ptb_producer(
+            data, batch_size, num_steps, name=name)
+
+
+# ## Config
+
+# In[22]:
+
+
+BASIC = "basic"
+CUDNN = "cudnn"
+BLOCK = "block"
+class TestConfig(object):
+    """Tiny config, for testing."""
+    init_scale = 0.1
+    learning_rate = 1.0
+    max_grad_norm = 1
+    num_layers = 1
+    num_steps = 2
+    hidden_size = 2
+    max_epoch = 1
+    max_max_epoch = 1
+    keep_prob = 1.0
+    lr_decay = 0.5
+    batch_size = 20
+    vocab_size = 10000 # 词的id数最大到9999
+    rnn_mode = BLOCK
+    
+config = TestConfig() # 训练时初始化用的config
+eval_config = TestConfig() # 验证时用的config
+eval_config.batch_size = 1
+eval_config.num_steps = 1
+
+
+# In[ ]:
+
+
+train_input = PTBInput(config=config, data=train_data, name="TrainInput")
+train_input.batch_size
+train_input.num_steps
+train_input.input_data.graph
+tf.get_default_graph()
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    res = sess.run(train_input.input_data)
+
+
+# In[ ]:
+
+
+res
+
+
+# In[63]:
+
+
+with tf.get_default_graph().as_default():
+    initializer = tf.random_uniform_initializer(-config.init_scale,
+                                                config.init_scale)
+
+    with tf.name_scope("Train"):
+        train_input = PTBInput(config=config, data=train_data, name="TrainInput")
+
+        #         with tf.variable_scope("Model", reuse=None, initializer=initializer):
+        #             m = PTBModel(is_training=True, config=config, input_=train_input)
+        #         tf.summary.scalar("Training Loss", m.cost)
+        #         tf.summary.scalar("Learning Rate", m.lr)
+
+        # 封装的模型PTBModel的初始化参数
+        _is_training = True
+        _input = train_input
+        config = config
+        # 数据的参数
+        batch_size = _input.batch_size
+        num_steps = _input.num_steps
+        # 模型结构
+        _rnn_params = None
+        _cell = None
+        # 超参数
+        size = config.hidden_size
+        vocab_size = config.vocab_size
+
+        # 构建embedding
+        with tf.device("/cpu:0"):
+            embedding = tf.get_variable("embedding", [vocab_size, size], dtype=tf.float32)
+            inputs = tf.nn.embedding_lookup(embedding, _input.input_data)
+        # 若在训练过程中，使用dropout
+        if _is_training and config.keep_prob < 1:
+            inputs = tf.nn.dropout(inputs, config.keep_prob)
+        # 构建rnn计算图
+        def make_cell():
+            cell = tf.contrib.rnn.LSTMBlockCell(config.hidden_size, forget_bias=0.0)
+            if is_training and config.keep_prob < 1:
+                cell = tf.contrib.rnn.DropoutWrapper(
+                    cell, output_keep_prob=config.keep_prob)
+            return cell
+        # MultiRNNCell | 多个LSTM内部结构前一个的输出是后一个的输入，inp -> lstm1 -> lstm2 -> lstm3 -> out
+        cell = tf.contrib.rnn.MultiRNNCell([make_cell() for _ in range(config.num_layers)], state_is_tuple=True)
+        # state状态 | 一开始MultiRNNCell都做全零初始化 zero_state
+        _initial_state = cell.zero_state(config.batch_size, tf.float32)
+        state = _initial_state
+        # outputs输出
+        outputs = []
+        with tf.variable_scope("RNN"):
+            for time_step in range(num_steps):
+                if time_step > 0: 
+                    tf.get_variable_scope().reuse_variables()
+                (cell_output, state) = cell(inputs[:, time_step, :], state)
+                outputs.append(cell_output)
+        output = tf.reshape(tf.concat(outputs, 1), [-1, config.hidden_size])
+        
+#         output, state = self._build_rnn_graph(inputs, config, is_training)
+        
+    with tf.name_scope("Valid"):
+        pass
+
+    with tf.name_scope("Test"):
+        pass
+
+
+# In[37]:
+
+
+with tf.Graph().as_default():
+    with tf.Session() as sess:
+    sess.run(train_input)
+    
+input_.input_data
 
 
 # In[ ]:
--- TagExtraction.py
+++ TagExtraction.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[2]:
+# In[1]:
 
 
 from IPython.core.interactiveshell import InteractiveShell
@@ -14,7 +14,7 @@ import copy,os,sys,psutil
 from collections import Counter
 
 
-# In[57]:
+# In[2]:
 
 
 import numpy as np
@@ -23,7 +23,7 @@ from zac_pyutils import ExqUtils
 from sklearn import metrics
 
 
-# In[4]:
+# In[3]:
 
 
 import tensorflow as tf
@@ -31,27 +31,74 @@ import tensorflow_hub as hub
 elmo = hub.Module("https://tfhub.dev/google/elmo/2", trainable = True)
 
 
-# In[6]:
+# In[4]:
 
 
 article = """Get News Updates directly on your WhatsApp. Click here to Subscribe.\n\nRanveer Singh and Deepika Padukone have been giving us major couple goals. And today, Deepika took to her Instagram to share her look for an award night.\n\n\n\n\n\nShe shared a series of photos from which can make anyone go weak in the knees. Ranveer also got awestruck after seeing the photos.DP looked absolutely stunning in a pink dress with a ruffled neck and Ranveer couldn't stop himself from commenting on the posts shared by the actress.\n\nAt the award function Deepika's father Prakash Padukone received Lifetime Achievement Award for his contribution in sports. Ranveer also attended the event to witness the moment.On the work front, Deepika will be next seen in Meghna Gulzar 's 'Chhapaak' while Ranveer's next is Kabir Khan's '83'."""
-sentences = [re.sub("\\n","",i.strip()) for i in article.strip().split(".")]
-sentences = [i for i in sentences if len(i)>0]
-sentences
 
 
-# In[74]:
+# # Prepare
+
+# ## split into sentences
+
+# In[5]:
 
 
-pad_len = 13
-demoSentence_total = ["Modi was appointed Chief Minister of Gujarat in 2001",
-                      "Narendra Modi was appointed Chief Minister of Gujarat in 2001",
-                      "Obama was appointed Chief Minister of Gujarat in 2001"]
-demoSentence_total = [" ".join(ExqUtils.padding(sen.split(" "),pad_len)) for sen in demoSentence_total]
-demoSentence_total
+def split_to_sentences(article):
+    sentences = [re.sub("\\n","",i.strip()) for i in article.strip().split(".")]
+    sentences = [i for i in sentences if len(i)>0]
+    return sentences
 
 
-# In[72]:
+# ## pad/truncate to same length
+
+# In[6]:
+
+
+def align(sentence_list, pad_len=13):
+    sentences_pad = [" ".join(ExqUtils.padding(sen.split(" "),pad_len)) for sen in sentence_list]
+    return sentences_pad
+
+
+# In[7]:
+
+
+padded_sens = align(split_to_sentences(article),pad_len=30)
+
+
+# In[21]:
+
+
+ExqUtils.zprint(f"""句子: {len(padded_sens)}, 词(each sentences): {len(padded_sens[0].split(" "))}""")
+padded_sens
+
+
+# # TF | Graph
+
+# In[22]:
+
+
+g0 = tf.get_default_graph()
+with g0.as_default():
+    emb_opt = elmo(inputs=padded_sens, as_dict=True)
+    word_emb = emb_opt['word_emb']
+    LM_emb = tf.concat([emb_opt['lstm_outputs1'], emb_opt['lstm_outputs2']],axis=2)
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        emb_res = sess.run(emb_opt)
+        LM_emb_res = sess.run(LM_emb)
+        
+
+
+# In[23]:
+
+
+emb_res['lstm_outputs1'].shape
+emb_res['lstm_outputs2'].shape
+LM_emb_res.shape # 10个句子，每个句子30个词，每个词1024维向量
+
+
+# In[ ]:
 
 
 with tf.Session() as sess:
@@ -62,7 +109,7 @@ with tf.Session() as sess:
     emb_demo_total = sess.run(emb_demo_opt)
 
 
-# In[11]:
+# In[ ]:
 
 
 
@@ -76,19 +123,19 @@ def cos_similarity(vec1,vec2):
     norm2 = tf.sqrt(tf.reduce_sum(tf.square(vec2), axis=2))
 
 
-# In[9]:
+# In[ ]:
 
 
 [len(i) for i in emb_sentence]
 
 
-# In[56]:
+# In[ ]:
 
 
 
 
 
-# In[8]:
+# In[ ]:
 
 
 ExqUtils.padding([12,123,14],5)
